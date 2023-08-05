40677956599d30cb7dbf77a0f5a1fa064fa71d72
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2023-08-05 21:59:03 +0630

    first upload

---
+++ database/migrations/2023_08_05_151851_create_posts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('photo');
+            $table->longText('description');
+            $table->date('post_date');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onDelete('cascade');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')
+                            ->references('id')
+                            ->on('users')
+                            ->onDelete('cascade');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('updated_by')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
